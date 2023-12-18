318af144edf84b17e989115d59f0aac9e789685d
Ana Santos <ana.br@hotmail.com>
2023-12-18 10:33:23 -0300

    Build

--- cch/src/components/About/About.js
+++ cch/src/components/About/About.js
@@ -1,35 +1,40 @@
-import React from 'react'
+import React from 'react';
 import Image from 'next/image';
-import styles from './about.module.css'
+import styles from './about.module.css';
 
 const About = () => {
   return (
     <section className={styles.aboutContainer}>
       <div className={styles.aboutInovation}>
-
         <div className={styles.aboutText}>
-
           <h1>Inovação Tecnológica para o seu Negócio</h1>
-          <p>Transforme a sua visão em realidade com a nossa experiência em desenvolvimento web e design.</p>
+          <p>
+            Transforme a sua visão em realidade com a nossa experiência em
+            desenvolvimento web e design.
+          </p>
 
           <div className={styles.buttons}>
-          <a href='#' className={styles.button1}>Quem Somos</a>
-          
-          <a href='#' className={styles.button2}>Contato</a>
+            <a href="#" className={styles.button1}>
+              Quem Somos
+            </a>
 
+            <a href="#" className={styles.button2}>
+              Contato
+            </a>
           </div>
-         
-
         </div>
 
         <div className={styles.aboutLogo}>
-            <Image className={styles.logoImage} src="/logo.png" width={418} height={435} />
+          <Image
+            className={styles.logoImage}
+            src="/logo.png"
+            width={418}
+            height={435}
+          />
         </div>
-
-
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default About
+export default About;
